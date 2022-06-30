06c2d8abb7c156ba13766e7686046893d773de5e
FredyRosero <faroseroc@unal.edu.co>
2022-06-30 04:20:46 -0500

    Path to root

--- src/app.js
+++ src/app.js
@@ -5,13 +5,13 @@ const root = require('./presentation/resolvers.js')
 const {port, env, endpoints} = require('./config.js')
 
 var app = express();
-app.use('/graphql', graphqlHTTP({
+app.use('/', graphqlHTTP({
   schema: schema.schema,
   rootValue: root.root,
   graphiql: env.development,
 }));
 app.listen(port);
-console.log(`Running GraphQL API server (${env.production?"production":"development"}) at http://localhost:${port}/graphql`);
+console.log(`Running GraphQL API server (${env.production?"production":"development"}) at http://localhost:${port}/`);
 if (env.development) {
   console.log("API MS endpoints:",endpoints)
 }
